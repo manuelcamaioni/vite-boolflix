a216ae212e7b9ded649ccbe4a86a302ef6491032
manuelcamaioni <camaioni.manuel@gmail.com>
2023-06-21 11:57:24 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <AppMain />
+</template>
+
+<script>
+// 256550ff1fdca66f331fdd0d6cec4eef API KEY
+import AppMain from "./components/AppMain.vue";
+export default {
+    components: {
+        AppMain,
+    },
+    data() {
+        return {};
+    },
+};
+</script>
+
+<style lang="scss">
+@import "bootstrap/scss/bootstrap";
+</style>
